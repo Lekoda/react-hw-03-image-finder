824517934cb887b16c08819f1de91b89a09fdf4b
Lekoda <kratorchestra@gmail.com>
2023-10-05 16:13:49 +0300

    Update API.js

--- src/components/Utils /API.js
+++ src/components/Utils /API.js
@@ -9,9 +9,12 @@
 //   return images;
 // }
 import axios from 'axios';
-
-axios.defaults.baseURL =
-  'https://pixabay.com/api/6272575-a6aecd500098fe7c64ad99f92';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '6272575-a6aecd500098fe7c64ad99f92';
 export const getImageData = async (query, page, perPage) => {
-  const response = await axios.get();
+  const response = await axios
+    .get(
+      `${axios.defaults.baseURL}?key=${API_KEY}&q=${query}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal`
+    )
+    .then();
 };
